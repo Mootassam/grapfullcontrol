0a04b6a824efa9bcaf446198adea5c03e2b94887
Mootassam <mootassame@gmail.com>
2024-03-27 14:31:08 +0400

    add the links

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,14 +1,14 @@
-import express from 'express';
-import cors from 'cors';
-import { authMiddleware } from '../middlewares/authMiddleware';
-import { tenantMiddleware } from '../middlewares/tenantMiddleware';
-import { databaseMiddleware } from '../middlewares/databaseMiddleware';
-import bodyParser from 'body-parser';
-import helmet from 'helmet';
-import { createRateLimiter } from './apiRateLimiter';
-import { languageMiddleware } from '../middlewares/languageMiddleware';
-import authSocial from './auth/authSocial';
-import setupSwaggerUI from './apiDocumentation';
+import express from "express";
+import cors from "cors";
+import { authMiddleware } from "../middlewares/authMiddleware";
+import { tenantMiddleware } from "../middlewares/tenantMiddleware";
+import { databaseMiddleware } from "../middlewares/databaseMiddleware";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import { createRateLimiter } from "./apiRateLimiter";
+import { languageMiddleware } from "../middlewares/languageMiddleware";
+import authSocial from "./auth/authSocial";
+import setupSwaggerUI from "./apiDocumentation";
 
 const app = express();
 
@@ -32,7 +32,7 @@ setupSwaggerUI(app);
 const defaultRateLimiter = createRateLimiter({
   max: 500,
   windowMs: 15 * 60 * 1000,
-  message: 'errors.429',
+  message: "errors.429",
 });
 app.use(defaultRateLimiter);
 
@@ -46,13 +46,13 @@ app.use(
   bodyParser.json({
     verify: function (req, res, buf) {
       const url = (<any>req).originalUrl;
-      if (url.startsWith('/api/plan/stripe/webhook')) {
+      if (url.startsWith("/api/plan/stripe/webhook")) {
         // Stripe Webhook needs the body raw in order
         // to validate the request
         (<any>req).rawBody = buf.toString();
       }
     },
-  }),
+  })
 );
 
 // Configure the Entity routes
@@ -60,19 +60,23 @@ const routes = express.Router();
 
 // Enable Passport for Social Sign-in
 authSocial(app, routes);
-require('./auditLog').default(routes);
-require('./auth').default(routes);
-require('./plan').default(routes);
-require('./tenant').default(routes);
-require('./file').default(routes);
-require('./user').default(routes);
-require('./settings').default(routes);
-require('./social').default(routes);
-require('./category').default(routes);
+require("./auditLog").default(routes);
+require("./auth").default(routes);
+require("./plan").default(routes);
+require("./tenant").default(routes);
+require("./file").default(routes);
+require("./user").default(routes);
+require("./settings").default(routes);
+require("./social").default(routes);
+require("./category").default(routes);
+require("./record").default(routes);
+require("./transaction").default(routes);
+require("./vip").default(routes);
+require("./product").default(routes);
 // Loads the Tenant if the :tenantId param is passed
-routes.param('tenantId', tenantMiddleware);
+routes.param("tenantId", tenantMiddleware);
 
 // Add the routes to the /api endpoint
-app.use('/api', routes);
+app.use("/api", routes);
 
 export default app;
